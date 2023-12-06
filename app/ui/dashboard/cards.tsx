15a7af8ae673690eb0add9744dadf925c5cbec64
ch2isk4kos <ch2isk4kos@gmail.com>
2023-12-05 19:43:10 -0500

    import fetchCardData method

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -4,6 +4,7 @@ import {
   UserGroupIcon,
   InboxIcon,
 } from '@heroicons/react/24/outline';
+import { fetchCardData } from '@/app/lib/data';
 import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
